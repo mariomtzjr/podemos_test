39977a07d1633840c3d0007971232fba5bfc40df
mariomtzjr <francisco.smtz@hotmail.com>
2020-05-28 11:37:47 -0500

    Se agregó directorio de ambiente virtual

---
+++ app_podemos_venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\mario_martinez\desktop\django_projects\podemos_progresar\app_podemos\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
